a8b5433aa571a2753479eeb4c45ef519c00ac48f
MUNDHE DATTATRAYA <149707964+CDCDattatraya@users.noreply.github.com>
2024-05-04 10:53:32 +0530

    Update Let and const

---
+++ 2. JS ES6 or ES2015/Let and const/block_scope.js
@@ -0,0 +1,23 @@
+//let and const can be accessed in this only 
+
+// var can be accessed globally 
+{
+	var a = 5;
+	let b = 7; 
+	const c = 16;
+	
+	console.log(a);
+	console.log(b);
+	console.log(c);
+}
+console.log(a);
+console.log(b);
+console.log(c);
+
+/*
+
+Block is defined by using { }. It is also called a compound statement in JavaScript.
+Block is used to combine multiple JavaScript statements into a group where JavaScript expect it as a statement.
+
+Block scope means all the variables can be accessed through out the block only.
+*/
