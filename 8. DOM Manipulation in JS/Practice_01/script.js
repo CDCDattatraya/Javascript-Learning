a52e3f9fa11d70f722eca0e0c72357676d634542
dattamundhe2410 <dattamundhe2410@gmail.com>
2024-06-07 21:10:46 +0530

    New JS Dom learning files added

---
+++ 8. DOM Manipulation in JS/Practice_01/script.js
@@ -0,0 +1,15 @@
+// DOM is programming interface
+// In JS there is document object which includes properties and methods.
+// We can capture user actions by using JS Events.
+
+let ela = document.getElementById('adc'); // node type object
+
+// Accessing HTML Element : DOM helps us to access HTML Element of webpage in the form of JS Object.
+let elb = document.getElementsByClassName("xyz");
+let elc = document.getElementsByTagName("li");
+let eld = document.querySelector("xyz");  // These are methods of DOM
+
+console.log(ela);
+
+// Properties of DOM
+
---
+++ 8. DOM Manipulation in JS/Practice_02/app.js
@@ -0,0 +1,12 @@
+
+
+
+
+// getElementById()
+const title = document.getElementById('main-heading');
+console.log(title);
+
+// getElementsByClassName()
+const lists = document.getElementsByClassName('list-item');
+console.log(lists);
+
